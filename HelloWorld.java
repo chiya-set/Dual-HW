99c16193fd7d4ba6022635c6db2837d05c1fdbbf
chiya-set <cilla.work135@gmail.com>
2026-04-26 19:39:21 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class HelloWorld extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage mainStage) {
+        mainStage.setTitle("Problem Ideation Form");
+
+        Button btn = new Button();
+        btn.setText("Print Hello World!");
+        btn.setOnAction(new CustomEventHandler());
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 300);
+        mainStage.setScene(scene);
+        mainStage.show();
+
+    }
+
+    private class CustomEventHandler implements EventHandler<ActionEvent> {
+        public void handle (ActionEvent event) {
+            System.out.println("Hello World!");
+        }
+    }
+}
